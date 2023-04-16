5604a817e1fcc9dc203d960df7030effac7412be
kazuhiro-na <narakhope@gmail.com>
2023-04-16 19:37:59 +0900

    productsテーブルを作成

---
+++ database/migrations/2023_04_16_100618_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('company_id')->constrained('companies');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
