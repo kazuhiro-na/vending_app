f6177cc576655bd48d7d816f5f4d313c041b11d3
kazuhiro-na <narakhope@gmail.com>
2023-04-18 01:37:11 +0900

    indexのルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/products', 'productsController@index');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
