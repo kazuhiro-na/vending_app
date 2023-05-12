443375d0acbd0e057cd44a46c627e2e91d631b63
kazuhiro-na <narakhope@gmail.com>
2023-05-12 12:18:21 +0900

    ルーティングを作成

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/products', 'productsController@index');
+//商品一覧表示画面のルート
+Route::get('/products', 'productsController@index')->name('products.index');
+//商品登録画面のルート
+Route::get('/products/create', 'ProductsController@create')->name('products.create');
+//商品登録処理のルート
+Route::post('/products', 'ProductsController@store')->name('products.store');
+//商品詳細表示画面のルート
+Route::get('/products/{product}', 'ProductsController@show')->name('products.show');
+//商品編集画面のルート
+Route::get('/products/{product}/edit', 'ProductsController@edit')->name('products.edit');
+//商品更新処理のルート
+Route::put('/products/{product}', 'ProductsController@update')->name('products.update');
+//商品削除処理のルート
+Route::delete('/products/{product}', 'ProductsController@destroy')->name('products.destroy');
 
 Auth::routes();
 
