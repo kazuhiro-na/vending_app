19ff211c77f31fea90d7a12b661494f18af04dba
kazuhiro-na <narakhope@gmail.com>
2023-05-15 10:49:05 +0900

    コントローラーに商品一覧、登録フォーム、商品登録のアクションを追加

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,14 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Company;
 
 class ProductsController extends Controller
-{
+{   
+    //商品一覧を表示
     public function index()
     {
-        $data = [
-            'products' => Product::all()
-        ];
-        return view('products.index', $data);
+        $products = Product::all();
+        return view('products.index', compact('products'));
     }
+    //商品登録フォームを表示
+    public function create()
+    {
+        $companies = Company::all();
+        return view('products.create', compact('companies'));
+    }
+    //商品を登録する
+
+    public function store(Request $request)
+    {
+        $product = new Product;
+        $product->company_id = $request->company_id;
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->stock = $product->stock;
+        $product->comment = $product->comment;
+        $product->save();
+
+        return redirect()->route('products.index');
+    }
+
+
 }
