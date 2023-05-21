53437366b2fcea97661195166b795a387c315a1b
kazuhiro-na <narakhope@gmail.com>
2023-05-21 20:43:54 +0900

    productsテーブルに画像カラムを追加

--- database/migrations/2023_04_16_100618_create_products_table.php
+++ database/migrations/2023_04_16_100618_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('image_path');
             $table->foreignId('company_id')->constrained('companies');
             $table->string('name');
             $table->integer('price');
