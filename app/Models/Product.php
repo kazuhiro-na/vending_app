7d93081585da186f7557ea90f00d04992f920640
kazuhiro-na <narakhope@gmail.com>
2023-06-15 22:39:43 +0900

    商品更新の関数を定義

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -38,4 +38,25 @@ class Product extends Model
     {
         return $this->belongsTo('App\Models\Company');
     }
+
+    public function updateProduct($id, $data)
+    {
+        $product = $this->findOrFail($id);
+
+        $product->name = $data['name'];
+        $product->company_id = $data['company_id'];
+        $product->price = $data['price'];
+        $product->stock = $data['stock'];
+        $product->comment = $data['comment'];
+
+        if (isset($data['image_path'])) {
+            $image = $data['image_path'];
+            $imagePath = $image->store('products', 'public');
+            $product->image_path = $imagePath;
+        }
+
+        $product->save();
+
+        return $product;
+    }
 }
