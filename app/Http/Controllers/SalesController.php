38047ddc19cc549bf513a0ebf52c6a96f996251c
kazuhiro-na <narakhope@gmail.com>
2023-07-14 16:52:35 +0900

    購入処理のアクションを追加

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Sale;
 use Illuminate\Http\Request;
 
 class SalesController extends Controller
 {
-    //
+    public function purchase(Request $request)
+    {
+        $productId = $request->input('product_id');
+        $quantity = $request->input('quantity');
+        $product = Product::find($productId);
+
+        if ($product && $product->stock >= $quantity) {
+            //在庫がある場合
+            $product->stock -= $quantity;
+            $product->save();
+            //売上の記録
+            $sale = new Sale();
+            $sale->product_id = $productId;
+            $sale->quantity = $quantity;
+            $sale->save();
+
+            return response()->json(['message' => config('messages.purchase')], 200);
+        } elseif ($product && $product->stock === 0) {
+            //在庫が0の場合
+            return response()->json(['message' => config('messages.purchase_0')], 400);
+        } else {
+            //商品が存在しない場合
+            return response()->json(['message' => config('messages.purchase_error')], 404);
+        }
+    }
 }
