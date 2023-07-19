2a183eff25a70774bc020d3e83e4d48331d5c0c7
kazuhiro-na <narakhope@gmail.com>
2023-07-19 23:17:02 +0900

    トランザクションを使用する記述に変更

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\Sale;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SalesController extends Controller
 {
@@ -14,23 +15,37 @@ class SalesController extends Controller
         $quantity = $request->input('quantity');
         $product = Product::find($productId);
 
-        if ($product && $product->stock >= $quantity) {
-            //在庫がある場合
-            $product->stock -= $quantity;
-            $product->save();
-            //売上の記録
-            $sale = new Sale();
-            $sale->product_id = $productId;
-            $sale->quantity = $quantity;
-            $sale->save();
-
-            return response()->json(['message' => config('messages.purchase')], 200);
-        } elseif ($product && $product->stock === 0) {
-            //在庫が0の場合
-            return response()->json(['message' => config('messages.purchase_0')], 400);
-        } else {
-            //商品が存在しない場合
+        //商品が存在しない場合
+        if(!$product) {
             return response()->json(['message' => config('messages.purchase_error')], 404);
+        }
+
+        DB::beginTransaction();
+        
+        try {
+            if ($product && $product->stock >= $quantity) {
+                //在庫がある場合
+                $product->stock -= $quantity;
+                $product->save();
+                //売上の記録
+                $sale = new Sale();
+                $sale->product_id = $productId;
+                $sale->quantity = $quantity;
+                $sale->save();
+
+                DB::commit();
+
+                return response()->json(['message' => config('messages.purchase')], 200);
+            } else {
+                //在庫が不足している場合
+                DB::rollBack();
+                return response()->json(['message' => config('messages.purchase_0')], 400);
+            }
+        } catch (\Exception $e) {
+            //例外が発生した場合
+            DB::rollBack();
+
+            return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
         }
     }
 }
